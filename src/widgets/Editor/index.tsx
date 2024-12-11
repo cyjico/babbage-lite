ab5e46b26f954d943ec8271bcc659de17b3e3cbe
cyjico <37214242+cyjico@users.noreply.github.com>
2024-12-11 18:27:11 +0800

    refactor: impl line creation/deletion

--- src/widgets/Editor/index.tsx
+++ src/widgets/Editor/index.tsx
@@ -1,7 +1,6 @@
-import getNextSibling from "@/shared/lib/getNextSibling";
-import getSelf from "@/shared/lib/getSelf";
 import "./styles.css";
 import { Accessor, For, Setter } from "solid-js";
+import createOnKeyDown from "./lib/createOnKeyDown";
 
 interface Props {
   breakpoints: Accessor<number[]>;
@@ -9,115 +8,11 @@ interface Props {
 }
 
 export default function Editor(props: Props) {
-  let ref_editorLines!: HTMLDivElement;
-
-  function onKeyDown(ev: KeyboardEvent) {
-    const sel = window.getSelection();
-    if (!sel || !sel.anchorNode) {
-      return null;
-    }
-    const selRange = sel.getRangeAt(0);
-
-    const nextSibling = getNextSibling(sel.anchorNode);
-
-    if (ev.key === "Enter") {
-      switch (sel.type) {
-        case "Caret":
-          {
-            const divElement = document.createElement("div");
-            divElement.classList.add("line");
-
-            const newLine =
-              selRange.endContainer.textContent?.slice(selRange.endOffset) ||
-              "";
-            if (newLine.length === 0) {
-              divElement.appendChild(document.createElement("br"));
-            } else {
-              const curLineElem = getSelf(selRange.endContainer);
-              const curLine =
-                curLineElem.textContent?.slice(0, selRange.endOffset) || "";
-
-              curLineElem.textContent = curLine;
-
-              if (curLine.length === 0) {
-                curLineElem.appendChild(document.createElement("br"));
-              }
-
-              divElement.textContent = newLine;
-            }
-
-            if (nextSibling) {
-              ref_editorLines.insertBefore(divElement, nextSibling);
-            } else {
-              ref_editorLines.appendChild(divElement);
-            }
-
-            selRange.setStart(divElement, 0);
-          }
-
-          ev.preventDefault();
-          break;
-        case "Range":
-          {
-            // Remove selected text
-            const endLineElem = getSelf(selRange.endContainer);
-            const endOffset = selRange.endOffset;
-
-            selRange.deleteContents();
-
-            if (selRange.commonAncestorContainer === ref_editorLines) {
-              // Selected text is multiline
-              if (endLineElem.textContent?.slice(endOffset).length === 0) {
-                endLineElem.appendChild(document.createElement("br"));
-              }
-
-              selRange.setStart(endLineElem, 0);
-            } else {
-              // Selected text is a single line
-              const divElement = document.createElement("div");
-              divElement.classList.add("line");
-
-              const newLine = endLineElem.textContent?.slice(endOffset) || "";
-              if (newLine.length === 0) {
-                divElement.appendChild(document.createElement("br"));
-              } else {
-                const curLineElem = endLineElem;
-                const curLine =
-                  curLineElem.textContent?.slice(0, endOffset) || "";
-
-                curLineElem.textContent = curLine;
-
-                if (curLine.length === 0) {
-                  curLineElem.appendChild(document.createElement("br"));
-                }
-
-                divElement.textContent = newLine;
-              }
-
-              if (nextSibling) {
-                ref_editorLines.insertBefore(divElement, nextSibling);
-              } else {
-                ref_editorLines.appendChild(divElement);
-              }
-
-              selRange.setStart(divElement, 0);
-            }
-
-            selRange.collapse(true);
-          }
-
-          ev.preventDefault();
-          break;
-        default:
-          console.warn("Unhandled selection type.", sel);
-          break;
-      }
-    }
-  }
+  let ref_content!: HTMLDivElement;
 
   return (
     <div class="editor">
-      <div class="editor__gutters">
+      <div class="gutters">
         <div class="gutter breakpoints">
           <For each={props.breakpoints()}>
             {(lineNumber) => {
@@ -136,13 +31,12 @@ export default function Editor(props: Props) {
       </div>
 
       <div
-        ref={ref_editorLines}
-        onKeyDown={onKeyDown}
+        ref={ref_content}
+        onKeyDown={createOnKeyDown(() => ref_content)}
         contentEditable="plaintext-only"
-        class="editor__lines"
+        class="content"
       >
         <div class="line">N000 10</div>
-        <div class="line">N</div>
         <div class="line">
           <br />
         </div>
