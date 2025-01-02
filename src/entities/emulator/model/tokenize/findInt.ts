111579295240570f6315b6fd6e4d20c0b3f33a6a
cyjico <37214242+cyjico@users.noreply.github.com>
2025-01-02 21:57:45 +0800

    feat: add length to findInt()

--- src/entities/emulator/model/tokenize/findInt.ts
+++ src/entities/emulator/model/tokenize/findInt.ts
@@ -1,9 +1,14 @@
 import isDigit from "./isDigit";
 
-export default function findInt(input: string, atStart: number) {
-  for (; atStart < input.length; atStart++) {
-    if (!isDigit(input[atStart])) break;
+export default function findInt(
+  input: string,
+  start: number,
+  length = Number.POSITIVE_INFINITY,
+) {
+  let i = start;
+  for (; i < input.length; i++) {
+    if (!isDigit(input[i]) || i - start >= length) break;
   }
 
-  return atStart;
+  return i;
 }
