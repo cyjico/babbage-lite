cbee22c3e7671756a2792cb6232054be08f76f25
cyjico <37214242+cyjico@users.noreply.github.com>
2025-01-05 03:05:15 +0800

    fix: handle negative numeric literals & edge cases

--- src/entities/emulator/model/lex/findEndOfDecimals.ts
+++ src/entities/emulator/model/lex/findEndOfDecimals.ts
@@ -1,17 +1,36 @@
 import { isDigit } from "./utils";
 
 export default function findEndOfDecimals(input: string, start: number) {
+  // a lone '-' will be lex'd as a keyword first before a numeric literal
+  const hasNegativeSign = input[start] === "-";
   let hasDecimalPoint = false;
-  for (; start < input.length; start++) {
-    if (input[start] === ".") {
+
+  let end = start + (hasNegativeSign ? 1 : 0);
+  for (; end < input.length; end++) {
+    if (hasNegativeSign && input[end] === "-") {
+      break;
+    }
+
+    if (input[end] === ".") {
       if (hasDecimalPoint) break;
 
       hasDecimalPoint = true;
       continue;
     }
 
-    if (!isDigit(input[start])) break;
+    if (!isDigit(input[end])) break;
   }
 
-  return start;
+  if (
+    // Only '-.' case
+    (hasNegativeSign && hasDecimalPoint && start + 2 === end) ||
+    // Only had '-' or '.'
+    ((hasNegativeSign || hasDecimalPoint) && start + 1 === end)
+  )
+    return start;
+
+  // '<number>.' or '-<number>.' case
+  if (hasDecimalPoint && input[end - 1] === ".") return end - 1;
+
+  return end;
 }
