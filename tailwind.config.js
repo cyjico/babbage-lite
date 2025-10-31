ce382f88ad8d2784ba6e9de0673ac78cf48643c0
cyjico <37214242+cyjico@users.noreply.github.com>
2025-10-31 23:16:35 +0800

    chore: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mygrey: '#323233',
+        mydarkgrey: '#252526',
+        myblack: '#1e1e1e',
+      },
+    },
   },
   plugins: [],
 };
